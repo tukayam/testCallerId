f1db0a57e783328f7b174d9b6c418d62753a22a0
Tuba Kaya <tuba_kaya@outlook.com>
2019-06-10 14:41:15 +0200

    try push

---
+++ Fabrikam.Core/PartitionKeyGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Fabrikam.Core
+{
+    public interface IPartitionKeyGenerator
+    {
+        string Get(string setId);
+    }
+
+    class PartitionKeyGenerator : IPartitionKeyGenerator
+    {
+        public string Get(string setId)
+        {
+            return $"setId:{setId}";
+        }
+    }
+}
---
+++ Fabrikam.Core/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fabrikam.Core
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAllInPartition(string partitionKey);
+    }
+
+    class Repository<T> : IRepository<T>
+    {
+        public IEnumerable<T> GetAllInPartition(string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
